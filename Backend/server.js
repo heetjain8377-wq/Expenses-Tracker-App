f196d7cb9b3853ec14fcb6ecafbe9d7ab1b2c407
heet jain <heetjain8377@gmail.com>
2026-07-16 00:16:21 +0530

    Adding Expenses Tracker App

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,8 @@ connectDB();
 app.use("/api/auth", userRoutes);
 app.use("/api/transaction", transactionRoutes);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
